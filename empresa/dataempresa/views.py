5e5d5036ce3cf45ce7b807b1bb73d1a02310f066
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-30 00:04:45 +0200

    Cambio de no error

--- empresa/dataempresa/views.py
+++ empresa/dataempresa/views.py
@@ -1,17 +1,17 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
-from .models import Coment
+from django.db import models
 # Create your views here.
 
 
 def creardatos(request):
-    empleadouno = Coment.objects.create(
+    empleadouno = models.objects.create(
     name="Bauty", comment="pablohurtado@gmail.com")
-    cargoempleadouno = Coment.objects.create(empleado=empleadouno,
+    cargoempleadouno = models.objects.create(empleado=empleadouno,
     headline='Prueba de instanciacion de claves foraneas',
     body_text='Si se crea con exito soy la prueba',
     rating=8)
 
-    resultadobusqueda = Coment.objects.get(id=1)
+    resultadobusqueda = models.objects.get(id=1)
 
     return resultadobusqueda
