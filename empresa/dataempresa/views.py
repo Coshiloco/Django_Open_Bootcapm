31eefcdde21cc3dd0a1236617c586f39863f2125
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-30 00:21:08 +0200

    Tiene que ser de modelo clase del import model

--- empresa/dataempresa/views.py
+++ empresa/dataempresa/views.py
@@ -1,17 +1,16 @@
-
 from django.shortcuts import render
-from django.db import models
+from .models import Cargo, Empleado
 # Create your views here.
 
 
 def creardatos(request):
-    empleadouno = models.objects.create(
+    empleadouno = Empleado.objects.create(
     name="Bauty", comment="pablohurtado@gmail.com")
-    cargoempleadouno = models.objects.create(empleado=empleadouno,
+    cargoempleadouno = Cargo.objects.create(empleado=empleadouno,
     headline='Prueba de instanciacion de claves foraneas',
     body_text='Si se crea con exito soy la prueba',
     rating=8)
 
-    resultadobusqueda = models.objects.get(id=1)
+    resultadobusqueda = Empleado.objects.get(id=1)
 
     return resultadobusqueda
